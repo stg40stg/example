ee505305f4eaf4603af22dfbffd6801797f86a1f
stg40 <stg40stg@gmail.com>
2019-12-12 21:18:15 +0300

    some change in script

--- script/slider.js
+++ script/slider.js
@@ -1,11 +1,15 @@
-var slides = require("../json/slides");
+const slides = [
+    { img: '../css/img/slide1.jpg', text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, voluptatem."},
+    { img: '../css/img/slide2.jpg', text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, voluptatem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, voluptatem."},
+    { img: '../css/img/slide3.jpg', text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, voluptatem.Lorem ipsum dolor sit amet, consecttur adipisicing elit. Tempora, voluptatem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, voluptatem."}
+];
 console.log(slides);
 
-const renderSlides = (img, text) =>`<div class="slide"><img src="${img}" alt="slide"></div>`;
+const renderSlides = (img, text) =>`<div class="slide"><img src="${img}" alt="slide"><p>${text}</p></div>`;
 
-const renderGoodsList = (list) => {
-    const goodsList = list.map(item => renderGoodsItem( item.id, item.name, item.price, item.button, item.img));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+const renderSlidesList = (list) => {
+    const slidesList = list.map(item => renderSlides( item.img, item.text));
+    document.querySelector('.main').innerHTML = slidesList.join('');
 };
 
-renderGoodsList(goods);
+renderSlidesList(slides);
